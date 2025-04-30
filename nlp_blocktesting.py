abc95eaf45f0e439411a39d45d7d6fd0ac18a958
Rithvik1124 <157668154+Rithvik1124@users.noreply.github.com>
2025-04-30 11:38:04 +0530

    Update nlp_blocktesting.py

--- nlp_blocktesting.py
+++ nlp_blocktesting.py
@@ -1,14 +1,18 @@
 import requests
+import re
 from bs4 import BeautifulSoup
-
-
-r = requests.get('https://www.smith-wesson.com')
-soup = BeautifulSoup(r.content, 'html.parser')
-s = soup.find('div')
-sentence = str(soup.find_all('p'))
 import spacy
 from pandas import *
 from spacy.pipeline import EntityRuler
+
+url=input('Enter the URL:')
+r = requests.get(url)
+pattern = r'"(.*?)"'
+matches = re.findall(pattern, r.text)
+sentence=" ".join([i for i in matches if len(i)>35 and "https" not in i])
+
+print(sentence)
+
 df=read_csv("words_dataset.csv")
 nlp = spacy.load('browser_model')
 doc = nlp(sentence)
@@ -16,18 +20,18 @@ total=0
 nsfw_count=0
 for i in doc.ents:
     total+=1
-    print(i.ent_,i.label_)
     if i.label_=='NSFW':
-        print(i.ent_,i.label_)
         nsfw_count+=1
 try:
-    if ((nsfw_count/total)*100)>30:
+    if ((nsfw_count/abs(total-nsfw_count))*100)>20:
+        print("NSFW:",nsfw_count)
+        print("Total:",total)
         print('Block this website.')
     else:
+        print("NSFW:",nsfw_count)
+        print("Total:",total)
         print('Website safe to surf')
 except ZeroDivisionError:
+    print("NSFW:",nsfw_count)
+    print("Total:",total)
     print('Website safe to surf')
-    
-
-
-
